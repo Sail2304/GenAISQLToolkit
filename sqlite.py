b891aec6a91e997639531d722dfd5211e33efeb0
Sail <gangan.sail@gmail.com>
2025-01-28 16:19:41 +0530

    added app.py

---
+++ sqlite.py
@@ -0,0 +1,35 @@
+import sqlite3
+
+
+## connect to sqlite
+connection=sqlite3.connect("student.db")
+
+# create cursor object to insert record, crate table
+cursor=connection.cursor()
+
+#create the table
+table_info="""
+create table STUDENT(NAME VARCHAR(25), CLASS VARCHAR(25), SECTION VARCHAR(25), MARKS INT)
+"""
+
+cursor.execute(table_info)
+
+## Inser some more records
+## Insert some more records
+cursor.execute('''Insert Into STUDENT values('Krish','Data Science','A',90)''')
+cursor.execute('''Insert Into STUDENT values('John','Data Science','B',100)''')
+cursor.execute('''Insert Into STUDENT values('Mukesh','Data Science','A',86)''')
+cursor.execute('''Insert Into STUDENT values('Jacob','DEVOPS','A',50)''')
+cursor.execute('''Insert Into STUDENT values('Dipesh','DEVOPS','A',35)''')
+
+
+## Display all the records
+print("The inserted records are")
+data=cursor.execute('''Select * from STUDENT''')
+
+for row in data:
+    print(row)
+
+## commit 
+connection.commit()
+connection.close()
